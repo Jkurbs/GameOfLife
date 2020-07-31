157d1a1bddbb46661012c3e6d613835ea7116237
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-30 21:49:53 -0400

    first commit

---
+++ GameOfLife/Models/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  GameOfLife (iOS)
+//
+//  Created by Kerby Jean on 7/30/20.
+//
+
+import Foundation
+
+struct Game {
+    var rules: [String]!
+    var about: String!
+}
---
+++ GameOfLife/View Controllers /MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  GameOfLife (iOS)
+//
+//  Created by Kerby Jean on 7/30/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
